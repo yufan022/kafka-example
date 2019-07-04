ec2edb44d758ed0a175931f3923f23190568b276
wangyufan <wangyufan@chainup.com>
2019-07-04 17:29:40 +0800

    init

---
+++ kafka-producer/src/main/java/info/yufan/producer/ProducerFastStart.java
@@ -0,0 +1,32 @@
+package info.yufan.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class ProducerFastStart {
+    public static final String brokerList = "yufan:9092";
+    public static final String topic = "topic-demo";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("bootstrap.servers", brokerList);
+
+
+        KafkaProducer<String, String> producer =
+                new KafkaProducer<>(properties);
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>(topic, "hello, Kafka!");
+        try {
+            producer.send(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        producer.close();
+    }
+}
